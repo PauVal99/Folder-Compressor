ade58f859ecf1bb26792a6d2fc1126e3ca9aa211
Sebas Rovira <becari@UPC-CFIS.UPC.EDU>
2019-11-13 17:45:25 +0100

    changes tester

--- tests/testers/LZWTester.java
+++ tests/testers/LZWTester.java
@@ -14,27 +14,31 @@ public class LZWTester {
     public void testVoid() 
     {
        tester.test("void","LZW");
+       tester.clean("void","LZW");
     } 
 
     public void testLine()
     {
         tester.test("line","LZW");
-
+        tester.clean("line","LZW");
     }
 
     public void testAscii()
     {
         tester.test("ascii","LZW");
+        tester.clean("ascii","LZW");
     }
 
     public void test4kb()
     {
         tester.test("4kb","LZW");
+        tester.clean("4kb","LZW");
     }
 
     public void test8kb()
     {
         tester.test("8kb","LZW");
+        tester.clean("8kb","LZW");
     }
 
 }
